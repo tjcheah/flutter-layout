685f5d0aede46e4dba4e47291ba6a2b6b52f6894
Tatiana-BeSquare <59550919+tjcheah@users.noreply.github.com>
2021-10-29 10:22:23 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -33,6 +33,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.amberAccent,
       appBar: AppBar(
         title: Text('Mock Data'),
       ),
